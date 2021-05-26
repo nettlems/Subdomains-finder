b5449efd45ee13a6509280d1ec78b0d516dd159e
nettlems <51298584+nettlems@users.noreply.github.com>
2021-05-27 02:44:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+
+# домен для поиска поддоменов
+domain = "github.com"
+
+# читать все поддомены
+file = open("subdomains-1000.txt")
+# прочитать весь контент
+content = file.read()
+# разделить на новые строки
+subdomains = content.splitlines()
+
+# список обнаруженных поддоменов
+discovered_subdomains = []
+for subdomain in subdomains:
+    # создать URL
+    url = f"http://{subdomain}.{domain}"
+    try:
+        # если возникает ОШИБКА, значит, субдомен не существует
+        requests.get(url)
+    except requests.ConnectionError:
+        # если поддомена не существует, просто передать, ничего не выводить
+        pass
+    else:
+        print("[+] Обнаружен поддомен:", url)
+        # добавляем обнаруженный поддомен в наш список
+        discovered_subdomains.append(url)
+
+# сохраняем обнаруженные поддомены в файл
+with open("discovered_subdomains.txt", "w") as f:
+    for subdomain in discovered_subdomains:
+        print(subdomain, file=f)
